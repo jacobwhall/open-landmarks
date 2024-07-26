85d1c339e93ffa9cd99c5dfa98c3cd203aaf41cc
jacobwhall <email@jacobhall.net>
2024-07-26 04:29:12 +0000

    Deploying to gh-pages from @ jacobwhall/open-landmarks@0b1380cc985ce3ef49f5871f43ee38d36848b4b8 🚀

--- build.js
+++ build.js
@@ -6,10 +6,7 @@ for (const inp of JSON.parse(fs.readFileSync("landmarks.json"))) {
   const resp = await fetch(
     `https://overpass-api.de/api/interpreter?data=[out:json];nwr[wikidata="${inp.wikidata}"]${inp.extra_qualifier || ""};out center;`,
   );
-  console.log(inp.wikidata);
-  const response = await resp.json();
-  console.log(response);
-  const { features: [feature] } = osmtogeojson(response);
+  const { features: [feature] } = osmtogeojson(await resp.json());
   features.push({ ...feature, properties: { ...feature.properties, ...inp } });
 };
 fs.writeFileSync("landmarks.geojson", JSON.stringify({ type: "FeatureCollection", features }));
